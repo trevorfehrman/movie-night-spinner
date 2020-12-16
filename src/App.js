f0f97597107c71519ab9be0b9d9b195f26fd5411
Trevor Fehrman <trevorfehrman@gmail.com>
2020-12-16 00:07:28 -0800

    poc

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
+import * as React from 'react';
+import styled from '@emotion/styled';
+import { motion, useMotionValue, useTransform } from 'framer-motion';
 import './App.css';
 
+const Container = styled('div')({
+  height: '100vh',
+  width: '100vw',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+});
+
+const Circle = styled(motion.div)({
+  height: '20rem',
+  width: '20rem',
+  backgroundColor: 'green',
+  borderRadius: '50%',
+});
+
+const Arrow = styled('div')({
+  height: '10rem',
+  width: '5px',
+  backgroundColor: 'black',
+  transform: 'translateX(10rem)',
+});
+
 function App() {
+  const [deg, setDeg] = React.useState(1000);
+
+  React.useEffect(() => {
+    console.log(deg);
+  }, [deg]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Circle
+        animate={{ rotate: deg }}
+        transition={{ duration: 2, ease: 'easeOut' }}
+        onPan={(e, panInfo) => {
+          // console.log(panInfo);
+          setDeg(panInfo.velocity.x * 2);
+        }}
+      >
+        <Arrow />
+      </Circle>
+    </Container>
   );
 }
 
